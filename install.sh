b37d2b880f2e3df4f84d516c085fd02351c4babd
RAKwireless <qi.zhu@rakwireless.com>
2020-01-08 15:00:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,5 +28,5 @@ sleep 1
 popd
 
 echo_success "*********************************************************"
-echo_success "*  The RAKwireless gateway is successfully installed!   *"
+echo_success "*  The RAKwireless for RAK2013 is successfully installed!   *"
 echo_success "*********************************************************"
